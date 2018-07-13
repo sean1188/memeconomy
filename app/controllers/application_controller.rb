a64dc6b3ed0eda88f41884f3490fff55dec82d53
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-13 10:34:19 +0800

    Add email authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
-    # Ensures all actions invoke this
-    before_action :authenticate_user!
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'landing/index'
-  get 'authentication/login'
-  get 'authentication/register'
+  get 'feed/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 end
