473fd83ad4c40dd1693a09a939b8a54bc1dc5ff0
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-25 16:41:26 +0800

    Fix user id missing from post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_025218) do
+ActiveRecord::Schema.define(version: 2018_07_25_082410) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,13 +58,8 @@ ActiveRecord::Schema.define(version: 2018_07_25_025218) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.text "description"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'refernces' for column 'user'
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -83,8 +78,8 @@ ActiveRecord::Schema.define(version: 2018_07_25_025218) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "name", default: "", null: false
     t.string "role"
+    t.string "name", default: "", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
