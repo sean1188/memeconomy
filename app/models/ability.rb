1d12195aff41a59a06c20f0d4c74943307d83869
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-26 10:35:07 +0800

    Add auth for post feed and show

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,9 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
+    user ||= User.new
     can :manage, :all if user.role == 'admin' 
-
+    can :manage, Post, :user_id => user.id
   end
-end
+  
+  end
