d5384f1d5bdd63f06635dba30433aff369519e05
Lau Cheng En <chengenlau4912@gmail.com>
2018-07-26 11:29:23 +0800

    Add user profile page

---
+++ app/controllers/userprofile_controller.rb
@@ -0,0 +1,5 @@
+class UserprofileController < ApplicationController
+	def index
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'landing/index'
   get 'feed/index'
   get 'admin/index'
+  get 'userprofile/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 end
