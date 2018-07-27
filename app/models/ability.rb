cee1cf6d4f82c65b89d58f9b5f88e22385b767f4
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-28 00:08:57 +0800

    Authorisation (#5)
    * Add auth for post feed and show
    
    * Fix user name missing
    
    * Deploy (#4)
    
    * Remove feed controller for deploy
    
    * Update gemfile
    
    * Switch database to psql
    
    * Update gemfile
    
    * Update production config
    
    * Update production config
    
    * Remove bootstrap
    
    * Update
    
    * Fix js compressor
    
    * Fix js compressor
    
    * Remove sqlite
    
    * Update databas config
    
    * Fix broken sign out route
    
    * Fix route
    
    * Switch host url
    
    * Add mailer config to production (oops)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,9 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
+    user ||= User.new
     can :manage, :all if user.role == 'admin' 
-
+    can %i[destroy update], Post, :user_id => user.id
   end
-end
+  
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get 'landing/index'
   get 'feed/index'
   get 'admin/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 end
+
