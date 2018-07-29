e27930b39370a61a07f74f355bea710d8b4bb4f5
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-29 21:38:21 +0800

    Fix post layout

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,8 @@ class Ability
     # Define abilities for the passed in user here. For example:
     user ||= User.new
     can :manage, :all if user.role == 'admin' 
-    can %i[destroy update], Post, :user_id => user.id
+    can %i(edit delete), Post, :user_id => user.id
     can :delete if user.role == 'moderator'
   end
   
-  end
+end
